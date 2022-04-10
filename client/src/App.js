0e26e1c7684f2c39a2d884c3d2054ba5c1e84cc8
Harun-techie53 <72112321+Harun-techie53@users.noreply.github.com>
2022-04-11 05:31:34 +0600

    setting up profile, post, auth and users api routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import api from './config';
+import React from 'react';
 import './App.css';
 
 const App = () => {
-    const [ text, setText ] = useState('');
-    const fetchResponse = async () => {
-       try {
-           const res = await api.get('/api/auth');
-            setText(res.data);
-       } catch (error) {
-           console.log(error);
-       }
-    }
-    useEffect(() => {
-        fetchResponse();
-    }, []);
     return (
         <div className='App'>
-            {text}
+            <h1>Hello World</h1>
         </div>
     )
 }
